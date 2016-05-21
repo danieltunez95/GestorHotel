e07cc6bf4ef117ae871bacbc15a2a0bacdc3a154
Daniel Túnez Bermejo <daniel.tunez95@gmail.com>
2016-05-21 18:19:10 +0200

    Generacion matriz + redirección en click

--- Gh.Presentation/web/Reserva.aspx.designer.cs
+++ Gh.Presentation/web/Reserva.aspx.designer.cs
@@ -49,24 +49,6 @@ namespace Gh.Presentation.web {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl plantaActual;
         
         /// <summary>
-        /// arrowUp control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton arrowUp;
-        
-        /// <summary>
-        /// arrowDown control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton arrowDown;
-        
-        /// <summary>
         /// hotelTable control.
         /// </summary>
         /// <remarks>
--- Gh.Presentation/web/Scripts/Reserva.js
+++ Gh.Presentation/web/Scripts/Reserva.js
@@ -1,20 +1,65 @@
-﻿function arrowUp_Click() {
-    planta++;
+﻿function siguientePlanta() {
+    PLANTA++;
+
+    CheckArrows();
+    Restart();
+    PrintHotel(PLANTA);
 }
 
-function arrowDown_Click() {
-    planta--;
+function anteriorPlanta() {
+    PLANTA--;
+
+    CheckArrows();
+    Restart();
+    PrintHotel(PLANTA);
 }
 
 
 function CheckArrows() {
+    if (PLANTA == ULTIMA_PLANTA)
+        document.getElementById("arrowUp").style.visibility = "hidden";
+    else
+        document.getElementById("arrowUp").style.visibility = "visible";
 
+    if (PLANTA == 0)
+        document.getElementById("arrowDown").style.visibility = "hidden";
+    else
+        document.getElementById("arrowDown").style.visibility = "visible";
 }
 
 function PrintHotel(planta) {
+    for (var i = 0; i < HOTEL.length; i++) {
+        var habitacion = HOTEL[i].replace("[", "").replace("]", "").split(",");
+        if (habitacion[0] == planta) {
+            document.getElementById(habitacion[2] + "_" + habitacion[1]).setAttribute("onClick", "Reenviar(this.id)");
+            if (habitacion[3] == 0)
+                document.getElementById(habitacion[2] + "_" + habitacion[1]).setAttribute("class", "celda libre");
+            else
+                document.getElementById(habitacion[2] + "_" + habitacion[1]).setAttribute("class", "celda ocupada");
 
+        }
+
+    }
+    //muestro la flecha hacia arriba
+    if (PLANTA != ULTIMA_PLANTA)
+        document.getElementById("arrowUp").style.visibility = "visible";
 }
 
+function Restart() {
+    //oculto todo
+    for (var x = 0; x <= LARGO; x++) {
+        for (var y = 0; y <= ANCHO; y++) {
+            document.getElementById(y + "_" + x).setAttribute("class", "celda invisible");
+        }
+    }
+}
+
+function Reenviar(id) {
+    window.location.replace("habitacion.aspx?id=" + id);
+}
 
 var HOTEL;
 var PLANTA = 0;
+var ULTIMA_PLANTA;
+var LARGO;
+var ANCHO;
