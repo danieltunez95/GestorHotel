ffc7b09562dd4ad0cb8649e34f395eca7fc8ef65
Víctor <vmmansilla93@gmail.com>
2016-05-22 20:46:48 +0200

    reservabus

---
+++ Gh.Bus/ReservaBus.cs
@@ -0,0 +1,45 @@
+﻿using Gh.Common;
+using Gh.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gh.Bus
+{
+    public class ReservaBus : IBus<ReservaDto>
+    {
+        ReservaDao dao = null;
+
+        public ReservaBus()
+        {
+            dao = new ReservaDao();
+        }
+
+        public ReservaDto Add(ReservaDto reserva)
+        {
+            return dao.Add(reserva);
+        }
+
+        public int Delete(ReservaDto reserva)
+        {
+            return dao.Delete(reserva);
+        }
+
+        public List<ReservaDto> GetAll()
+        {
+            return dao.GetAll();
+        }
+
+        public ReservaDto GetById(int id)
+        {
+            return dao.GetById(id);
+        }
+
+        public int Update(ReservaDto reserva)
+        {
+            return dao.Update(reserva);
+        }
+    }
+}
